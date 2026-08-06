36c0bab2f545d6d897dccb3de2fa5a0d2e74babb
Suneet Reddy <suneetreddy.p@gmail.com>
2026-08-06 23:51:38 +0530

    Added customer feature engineering

--- customer_segmentation.py
+++ customer_segmentation.py
@@ -12,3 +12,27 @@ print(df.columns)
 
 print("\nMissing Values:")
 print(df.isnull().sum())
+
+import pandas as pd
+
+df = pd.read_csv("Superstore.csv", encoding="latin1")
+
+df = df.dropna()
+
+customer_data = df.groupby("Customer ID").agg(
+    {
+        "Sales":"sum",
+        "Profit":"sum",
+        "Quantity":"sum",
+        "Order ID":"count"
+    }
+)
+
+customer_data.columns=[
+    "Total Sales",
+    "Total Profit",
+    "Total Quantity",
+    "Total Orders"
+]
+
+print(customer_data.head())
